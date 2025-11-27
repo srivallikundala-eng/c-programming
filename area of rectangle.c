29f5d9ac3b2d1c66921a015373833b08fa7344a3
sreevallikundala <srivallikundala@gmail.com>
2025-11-27 21:45:08 +0530

    Add files via upload

---
+++ area of rectangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	float length,breadth,area;
+	printf("enter length,breadth values");
+	scanf("%f%f",&length,&breadth);
+	area=length*breadth;
+	printf("area=%f",area);
+	return 0;
+}
