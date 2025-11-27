4563f31a3b664a70387504dad4f864aa64ac2353
sreevallikundala <srivallikundala@gmail.com>
2025-11-27 21:40:56 +0530

    Add files via upload

---
+++ bio data.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	printf("name:sreevalli");
+	printf("dob:21 jan 2009");
+	printf("college:aditya university");
+	printf("branch:AIML-7");
+	printf("father name:sathibabu");
+	printf("mother name:appalaraju");
+	printf("brother name:veerababu");
+	printf("sister name:mani");
+	return 0;
+}
