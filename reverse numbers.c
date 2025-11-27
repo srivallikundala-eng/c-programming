ce917946638ef58771415c50981eeb6c09d71e6b
sreevallikundala <srivallikundala@gmail.com>
2025-11-27 21:58:16 +0530

    Add files via upload

---
+++ reverse numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n,rev=0,d;
+	printf("enter any value");
+	scanf("%d",&n);
+	while(n>0)
+	{
+		d=n%10;
+		rev=rev*10+d;
+		n=n/10;
+	}
+	printf("reverse=%d",rev);
+	return 0;
+}
+	
+
