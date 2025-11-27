b3310647ba3aa2adc45126616854bba752279aef
sreevallikundala <srivallikundala@gmail.com>
2025-11-27 21:44:12 +0530

    Add files via upload

---
+++ average of three numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	printf("enter a value");
+	scanf("%d",&a);
+	printf("enter b value");
+	scanf("%d",&b);
+	printf("enter d value");
+	scanf("%d",&d);
+	c=(a+b+d);
+	printf("sum of three numbers=%d",c);
+	printf("\n division=%d",c/3);
+	return 0;
+}
