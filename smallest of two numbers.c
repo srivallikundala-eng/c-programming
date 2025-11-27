f77d3f65ee0e5777d9b4847e5a849f34e4cf19ab
sreevallikundala <srivallikundala@gmail.com>
2025-11-27 21:54:06 +0530

    Add files via upload

---
+++ smallest of two numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter a,b values");
+	scanf("%d%d",&a,&b);
+	if(a<b)
+	{
+		printf("a is small");
+	}
+	else
+	{
+		printf("b is small");
+	}
+	return 0;
+}
